65d2eb6fda0cf59ec6423a131215a7d39105495a
HussainQadri <hussain.bqadri@gmail.com>
2026-02-28 23:26:42 +0000

    FIXParser class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello World!";
-    return 0;
-}
+class FIXMessage {
+  private:
+    std::string symbol;
+    float price;
+    int quantity;
+    std::string side;
+
+    FIXMessage(std::string symbol, float price, int quantity,
+               std::string side) {
+        this->symbol = symbol;
+        this->price = price;
+        this->quantity = quantity;
+        this->side = side;
+    }
+};
+
+void Parse(FIXMessage message) {}
+
+int main() { return 0; }
