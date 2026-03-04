9f16626e6fc0dbc4a6ec06eec94da900cd87c5df
Hussain Qadri <hussain.bqadri@gmail.com>
2026-03-04 10:57:11 +0000

    Checksum validation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using std::cout;
 using std::string;
 
 class FIXMessage {
@@ -50,9 +51,45 @@ std::vector<std::pair<string, string>> Parse(string rawFixString) {
     return message;
 }
 
+std::pair<string, string>
+extractChecksumPair(std::vector<std::pair<string, string>> parsedMessage) {
+
+    std::pair<string, string> currentPair = parsedMessage.at(0);
+    int i = 0;
+    while (currentPair.first != "10") {
+        i = i + 1;
+        currentPair = parsedMessage.at(i);
+    }
+    return currentPair;
+}
+
+int totalBytes(std::vector<std::pair<string, string>> parsedMessage) {
+    std::pair<string, string> currentPair = parsedMessage.at(0);
+    int i = 0;
+    int byteValue = 0;
+
+    while (currentPair.first != "10") {
+        for (char c : currentPair.first) {
+            byteValue += static_cast<int>(c);
+        }
+
+        for (char c : currentPair.second) {
+            byteValue += static_cast<int>(c);
+        }
+
+        i = i + 1;
+        currentPair = parsedMessage.at(i);
+    }
+    return byteValue;
+}
+bool validate(std::pair<string, string> checkSumPair, int byteTotal) {
+    int checkSum = stoi(checkSumPair.second);
+    return checkSum == byteTotal;
+}
+
 int main() {
     string testMessage = "8=FIX.4.2|9=49|35=0|49=SENDER|56=TARGET|34=1|52="
-                         "20231024-15:30:00.000|10=161|";
+                         "20231024-15:30:00.000|10=3220|";
     std::vector<std::pair<string, string>> parseResult = Parse(testMessage);
 
     for (std::pair<string, string> pair : parseResult) {
@@ -60,8 +97,16 @@ int main() {
                   << "\n";
     }
 
-    FIXMessage fixMessage(parseResult);
-    std::cout << fixMessage.getTagAtIndex(0);
+    auto checkSumPair = extractChecksumPair(parseResult);
+    int byteValue = totalBytes(parseResult);
+
+    cout << "Checksum Tag: " << checkSumPair.first
+         << " Checksum value: " << checkSumPair.second
+         << " Byte total: " << byteValue << std::endl;
+
+    cout << "Valid: " << validate(checkSumPair, byteValue);
+
+    FIXMessage fixMessage(parseResult); // useless - change later
 
     return 0;
 }
