ac09d66faa020726e7de568bca34e8e977aa49ff
HussainQadri <hussain.bqadri@gmail.com>
2026-04-09 15:42:53 +0100

    Heartbeat basic header

--- src/Heartbeat.cpp
+++ src/Heartbeat.cpp
@@ -1,11 +1,15 @@
 #include "Heartbeat.h"
 
-std::string Heartbeat::msgType() const {
-    return "0";
+Heartbeat::Heartbeat()
+    : m_mandatoryTags{"8", "9", "35", "49", "56", "34", "52", "10"} {
 }
 
 bool Heartbeat::validate() const {
     // will actually implement, validate will check if header and trailer
     // contains the required fields for header and trailer.
     return true;
+}
+
+std::string Heartbeat::msgType() const {
+    return "0";
 }
--- src/Heartbeat.h
+++ src/Heartbeat.h
@@ -1,8 +1,15 @@
 #pragma once
 #include "TypedMessage.h"
-
+#include <set>
+#include <string>
+#include <unordered_map>
 class Heartbeat : TypedMessage {
 public:
+    Heartbeat();
     std::string msgType() const override;
     bool validate() const override;
+    std::unordered_map<std::string, std::string> requiredKeyValuePairs;
+
+private:
+    std::set<std::string> m_mandatoryTags;
 };
